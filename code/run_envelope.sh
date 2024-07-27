382ecf3d6f0a0e1cab1122a38cd70406aff61524
Panacea Soft <teamps.gitlab.mobile.dev@gmail.com>
2024-07-27 09:26:08 -0700

    Added ada, avax and sol

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,2 +1,5 @@
 source Algo-Trading-Bot/code/.venv/bin/activate
 python3 Algo-Trading-Bot/code/strategies/envelope/run.py
+python3 Algo-Trading-Bot/code/strategies/envelope/run_ada.py
+python3 Algo-Trading-Bot/code/strategies/envelope/run_avax.py
+python3 Algo-Trading-Bot/code/strategies/envelope/run_sol.py
